526b4ca30a7c8b8b270a820e03bba150f81530c6
Jimmy Bogard <jimmy.bogard@gmail.com>
2025-12-03 13:51:34 -0600

    Switching build to pure pwsh

--- psake.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env pwsh
-
-# Helper script for those who want to run psake from cmd.exe
-
-invoke-psake @args; if ($psake.build_success -eq $false) { exit 1 } else { exit 0 }
--- setup.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-rem Helper script for those who want to run psake from cmd.exe
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "./setup.ps1"
